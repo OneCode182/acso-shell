4a0b579b4217e25cc80c5721631eddbcb51c7716
OneCode182 <onecode0182@gmail.com>
2024-10-03 12:07:06 -0500

    Adding

---
+++ Windows/point_1.ps1
@@ -0,0 +1,9 @@
+# Clear the screen
+Clear-Host
+
+# Print "Hola Mundo" to the screen
+Write-Host "Hello World from Shell"
+
+# Pause the script waiting for Enter
+Write-Host "Press Enter to continue..." 
+Read-Host
